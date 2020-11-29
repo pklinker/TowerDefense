64f07c56e547791a49f24ac3fc9a3af10a681ba9
pklinker <pklinker@gmail.com>
2020-11-28 19:54:27 -0500

    Cannons now shoot and blow up enemy.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    AudioSource audioSource;
+    Collider boxCollider;
+   // ScoreBoard scoreBoard;
+
+    [Header("Effects")]
+    [Tooltip("Death explose sound effect")] [SerializeField] AudioClip deathExplosionAC;
+    [SerializeField] GameObject deathExplosion;
+    [SerializeField] Transform parent;
+
+    [Header("Scoring")]
+    [Tooltip("Points to add to the player's score")] [SerializeField] int weaponStrength = 1;
+    [Tooltip("Enemy resiliance to damage")] [SerializeField] int healthPoints = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        audioSource = GetComponent<AudioSource>();
+        AddCollider();
+       // scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
+
+    private void AddCollider()
+    {
+        AddNonTriggerBoxCollider();
+    }
+
+    void AddNonTriggerBoxCollider()
+    {
+        boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        healthPoints--;
+        if (healthPoints <= 0)
+        {
+          //  scoreBoard.ScoreHit(weaponStrength);
+            GameObject fx = Instantiate(deathExplosion, transform.position, Quaternion.identity);
+            fx.transform.parent = parent;
+            //        print("Particles collided with enemy " + gameObject.name);
+            Destroy(gameObject);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [SerializeField] Transform objectToPan;
+    [SerializeField] Transform targetEnemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TargetEnemy();
+    }
+
+    void TargetEnemy()
+    {
+        objectToPan.LookAt(targetEnemy);
+    }
+}
